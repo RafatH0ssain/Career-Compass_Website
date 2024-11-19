78fa75e741cac08e706facedc5627475c3b4015b
RafatH0ssain <rafat.click.hossain@gmail.com>
2024-11-19 18:54:43 -0400

    Added File Structure

---
+++ career-compass-viteproj/src/routes/router.jsx
@@ -0,0 +1,35 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        // element: <HomeLayout />,
+    },
+    {
+        path: '/services/:id',
+        // element: <PrivateRoute>
+        //     <NewsDetails />
+        // </PrivateRoute>,
+        // loader: ({ params }) => fetch(`https://openapi.programming-hero.com/api/news/${params.id}`),
+    },
+    {
+        path: '/auth',
+        // element: <AuthLayout />,
+        // children: [
+        //     {
+        //         path: '/auth/login',
+        //         element: <Login />
+        //     },
+        //     {
+        //         path: '/auth/register',
+        //         element: <Register />
+        //     },
+        // ],
+    },
+    {
+        path: "*",
+        element: <h1>Error</h1>
+    },
+]);
+
+export default router;
